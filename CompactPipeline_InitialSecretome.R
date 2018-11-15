3cc7d87c041ebc0ee58b554b3b8bd9a6de35252c
Paola Rojas Estevez <paolarojasestevez@gmail.com>
2018-11-14 22:15:26 -0500

    change input files and signalp versions to use.

--- CompactPipeline_InitialSecretome.R
+++ CompactPipeline_InitialSecretome.R
@@ -6,7 +6,7 @@
 ###############################################
 
 library(SecretSanta)
-Load_fasta<-readAAStringSet("/home/paola/1.Masterproyect/proteinasPredichasPbetacei.fasta")
+Load_fasta<-readAAStringSet("/home/paola/1.Masterproyect/04_MAKER/P8084.finalAssembly.all.maker.augustus_masked.proteins.fasta")
 
 #To Cluster
 #load_fasta<-readAAStringSet("/hpcfs/home/cp.rojas/MasterProyect_effectors/SecretSanta_ResultGogleva/Archivos_Gogleva
@@ -14,8 +14,10 @@ Load_fasta<-readAAStringSet("/home/paola/1.Masterproyect/proteinasPredichasPbeta
 
 CBSResult_input_SignalP <-CBSResult(in_fasta = Load_fasta)
 
-pipeline_all <- signalp(CBSResult_input_SignalP, version = 4, organism = 'euk', run_mode = 'starter') %>% 
-  tmhmm(TM = 0) %>%
+
+pipeline_all <- signalp(CBSResult_input_SignalP, version = 2, organism = 'euk', run_mode = 'starter',legacy_method = 'hmm' ) %>% 
+  signalp(version = 3, organism = 'euk', run_mode = 'piper',legacy_method = 'hmm') %>% tmhmm(TM = 0) %>%
   targetp(network = 'N', run_mode = 'piper') %>%
   check_khdel(pattern = 'prosite')
 writeXStringSet(getOutfasta(pipeline_all), 'secretome_out.fasta')
+
--- CompactPipeline_RescuedSecretome_Gogleva.R
+++ CompactPipeline_RescuedSecretome_Gogleva.R
@@ -6,7 +6,7 @@
 ###############################################
 
 library(SecretSanta)
-Load_fasta<-readAAStringSet("/home/paola/1.Masterproyect/Dataset1/LILI_transcriptome_V5_transdecoder.pep_mod_withoutStopCodonSymbol.fasta")
+Load_fasta<-readAAStringSet("/home/paola/1.Masterproyect/04_MAKER/P8084.finalAssembly.all.maker.augustus_masked.proteins.fasta")
 
 #To Cluster
 #load_fasta<-readAAStringSet("/hpcfs/home/cp.rojas/MasterProyect_effectors/SecretSanta_ResultGogleva/Archivos_Gogleva
@@ -15,14 +15,12 @@ Load_fasta<-readAAStringSet("/home/paola/1.Masterproyect/Dataset1/LILI_transcrip
 CBSResult_input_SignalP <-CBSResult(in_fasta = Load_fasta)
 
 
-pipeline_Rescued <- m_slicer(Load_fasta, # a set of amino acid sequences
-                                             run_mode = 'slice',
-                                             min_len = 100)
-  
-  
-  
-  signalp(CBSResult_input_SignalP, version = 4, organism = 'euk', run_mode = 'starter') %>% 
-  tmhmm(TM = 0) %>%
+pipeline_Rescued <- m_slicer(Load_fasta,run_mode = 'slice',min_len = 100) %>%
+  signalp(CBSResult_input_SignalP, version = 2, organism = 'euk', run_mode = 'starter',legacy_method = 'hmm' ) %>% 
+  signalp(version = 3, organism = 'euk', run_mode = 'piper') %>% tmhmm(TM = 0) %>%
   targetp(network = 'N', run_mode = 'piper') %>%
   check_khdel(pattern = 'prosite')
-writeXStringSet(getOutfasta(pipeline_all), 'secretome_out.fasta')
+writeXStringSet(getOutfasta(pipeline_all), 'secretome_RESCUED_out.fasta') 
+  
+  
+  
